d172d51c5385a982d7fb2ed3937969536dd2ba1d
Marco Barenghi <marco.barenghi.96@gmail.com>
2021-06-07 15:10:57 +0200

    Code was cleaned. Also, now the user is able to input number of days she/he wants to plot

--- Python/PlotCovid/functions.py
+++ Python/PlotCovid/functions.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-def userInput(userList):
-	n = input("How many countries do you want to get information about? ")
-	n = int(n) 
-		
+def userInput():
+	userList = []
+	nDays = 0
+	n = input("  How many countries do you want to get information about? ")
+	n = int(n)	
 	if (n > 0):
 		for i in range(n):
 			answer = str()
-			print("Select country #", i+1)
-			answer = input()
+			answer = input("  Select country #" + str(i+1) + ": ")
 			userList.append(answer)
 	else:
-		print("Only EU data will be displayed")
+		print("  Only EU data will be displayed")
 		
-def covidStats(List):
-	print("\n GETTING DATA! Please wait a few secs")
+	nDays = input("  Display number of days: ")
+	nDays = int(nDays)
+	
+	return userList,nDays
+		
+def covidStats(List, nDays):
+	print("\n  GETTING DATA! Please wait a few secs")
 	List.append("European Union")
 	df = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv")
 	df2 = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
@@ -33,27 +36,25 @@ def covidStats(List):
 	for country in List:
 		mask = data[:,0] == country #filter per country
 		data_with_mask =data[mask]
-		dates = data_with_mask[:,1][:-1]
-		nPoints = len(dates)
-		startingDate = dates[0]
-		cumuVax = data_with_mask[:,2][:-1]
+		cumuVax = data_with_mask[:,2][-nDays-1:-1]
 		mask2 = data2[:,0] == country
 		data_with_mask2 =data2[mask2]
-		newCases = data_with_mask2[:,2][-nPoints-1:-1]
+		newCases = data_with_mask2[:,2][-nDays-1:-1]
 		
 		#print some info
 		print("\n", "-----",country, "-----")
-		print(f"Total vaccinations per hundred: {float(data_with_mask[-1:,2][0])}")
-		print(f"Vaccinated people per hundred: {float(data_with_mask[-1:,3][0])}")
-		print(f"Fully vaccinated people per hundred: {float(data_with_mask[-1:,4][0])}")
+		print(f"  Total vaccinations per hundred: {float(data_with_mask[-1:,2][0])}")
+		print(f"  Vaccinated people per hundred: {float(data_with_mask[-1:,3][0])}")
+		print(f"  Fully vaccinated people per hundred: {float(data_with_mask[-1:,4][0])}")
 		
-		ax1.plot(range(nPoints), cumuVax, label=country)
+		#plot
+		ax1.plot(range(-nDays,0), cumuVax, label=country)
 		ax1.legend(loc='upper left');
 		ax1.set_title("Vaccinated people (per hundred)", weight="bold")
-		ax2.plot(range(nPoints), newCases, label=country)
+		ax2.plot(range(-nDays,0), newCases, label=country)
 		ax2.set_title("Smoothed new cases (per million)",weight="bold")
-		ax2.legend(loc='upper left');
+		ax2.legend(loc='upper right');
 		
-	fig.suptitle('Vaccinations and new cases from ' + str(startingDate), size=12, style='italic')	
+	fig.suptitle('Vaccinations and new cases in the last ' + str(nDays) + ' days', size=12, style='italic')	
 	plt.show()
 	
--- Python/PlotCovid/main.py
+++ Python/PlotCovid/main.py
@@ -3,9 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from functions import covidStats, userInput
 
-print(" \nPlotting and comparing vaccinations!")
+print("\n  Covid19 data visualization! \n")
 
 #List = ["Germany","Austria", "Italy"]
 List2 = []
-userInput(List2)
-covidStats(List2)
+nDays = 0
+List2, nDays = userInput()
+covidStats(List2,nDays)
