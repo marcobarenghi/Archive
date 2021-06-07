4b7b77621a3b1a068c222dbd0d2c8cb08295e749
Marco Barenghi <marco.barenghi.96@gmail.com>
2021-06-07 09:19:57 +0200

    Updated version. Code was cleaned and buffer manipulation was made more efficient. Old eta array m_eta was deleted

--- C++/RingBuffer/buff.h
+++ C++/RingBuffer/buff.h
@@ -9,15 +9,15 @@ public:
 	buffer(int num);
 	~buffer();
 	void Put(T add);
-	void Print(bool eta);
+	void Print();
 	T Get();
 	int Capacity() const;
 	int Size() const;
 
 private:
 	int m_capacity, m_currentSize, m_headPos;
+	bool m_wasFull;
 	T* m_array;
-	int *m_eta; //measures eta of all added elements -> needed to find m_headPos each time
 };
 
 template <typename T>
@@ -25,21 +25,21 @@ buffer<T>::buffer(){
 	m_capacity = 0;
 	m_currentSize = 0;
 	m_headPos = 0;
+	m_wasFull = false;
 	};
 
 template <typename T>
 buffer<T>::buffer(int num){
 	m_array = new T[num];
-	m_eta = new int[num];
 	m_capacity = num;
 	m_currentSize = 0;
 	m_headPos = 0;
+	m_wasFull = false;
 };
 
 template <typename T>
 buffer<T>::~buffer(){ 
 	delete[] m_array;
-	delete[] m_eta;
 };
 
 template <typename T>
@@ -47,70 +47,34 @@ void buffer<T>::Put(T add){
 	if (m_currentSize < m_capacity)
 	{
 		m_array[m_currentSize] = add;
-		for (int i = 0; i < m_currentSize; i++)
-		{
-			m_eta[i]++;
-		}
 		
-		int maxEta = -1;
-		for (int i = 0; i < m_currentSize; i++)
-		{
-			if( maxEta < m_eta[i])
-			{
-				maxEta = m_eta[i];
-				m_headPos = i;
-			}
-		}
+		m_headPos = 0;
 		m_currentSize++;
 		
 	}
 	else if (m_currentSize == m_capacity)
 	{
-		m_eta[m_headPos] = 0;
+		m_wasFull = true;
 		m_array[m_headPos] = add;
-		for (int i = 0; i < m_currentSize; i++)
-		{
-			if(i != m_headPos)
-			{
-			m_eta[i]++;
-			}
-		}
-		
-		int maxEta = -1;
-		for (int i = 0; i < m_currentSize; i++)
-		{
-			if( maxEta < m_eta[i])
-			{
-				maxEta = m_eta[i];
-				m_headPos = i;
-			}
-		}	
+		if (m_headPos < m_capacity - 1)
+			m_headPos++;
+		else
+			m_headPos = 0;
+			
 	}
 }
 
 template <typename T>
-void buffer<T>::Print(bool printeta){
+void buffer<T>::Print(){
 	if (m_currentSize == 0)
-	{
-		std::cout << "BUFFER IS EMPTY: CAN'T PRINT" << std::endl;
-	}
-	
-	if (m_currentSize > 0 && printeta==false)
-	{
-		for (int i = 0; i < m_currentSize; i++)
-		{
-			std::cout << m_array[i] << " ";
-		}
-		std::cout << std::endl;
-	}
-	else if (m_currentSize > 0 && printeta==true)
-	{
+		std::cout << "  Print(): empty buffer!" << std::endl;
 
+	else
+	{
+		std::cout << "  ";
 		for (int i = 0; i < m_currentSize; i++)
-		{
-			std::cout << m_array[i]  << " (" << m_eta[i] << ") ";
-		}
-		std::cout << std::endl;
+			std::cout << m_array[i] << " ";
+		std::cout <<std::endl;
 	}
 }
 
@@ -118,7 +82,7 @@ template <typename T>
 T buffer<T>::Get(){	
 	if (m_currentSize == 0)
 	{
-		std::cout << "Buffer is empty. Can't get value!" << std::endl;
+		std::cout << "  Get(): WARNING! Buffer is empty. NULL value will be returned!" << std::endl;
 		return {};
 	}
 		
@@ -132,34 +96,26 @@ T buffer<T>::Get(){
 			tmp_array[counttmp] = m_array[i];
 			counttmp++;
 		}
-    }  
-    delete[] m_array;
-    m_array = tmp_array;
+    }
     
-    int *tmp_eta = new int();  
-	int counttmpeta = 0;
-    for (int i = 0; i < m_currentSize; i++)
+    if (m_wasFull == true)
     {
-		if (i != m_headPos)
-		{
-			tmp_eta[counttmpeta] = m_eta[i];
-			counttmpeta++;
-		}
-    }  
-    delete[] m_eta;
-    m_eta = tmp_eta;
+		if (m_headPos == m_currentSize - 1)
+			m_headPos = 0;
+
+		m_wasFull = false;
+	}
+	else
+		m_headPos = 0;
+	
+     
+    delete[] m_array;
+    m_array = tmp_array;   
 	m_currentSize--;
 	
-	int maxEta = -1;
-		for (int i = 0; i < m_currentSize; i++)
-		{
-			if( maxEta < m_eta[i])
-			{
-				maxEta = m_eta[i];
-				m_headPos = i;
-			}
-		}
-	
+	if (m_currentSize == 0)
+		m_wasFull = false;
+
 	return tmpVal;
 }
 
--- C++/RingBuffer/main.C
+++ C++/RingBuffer/main.C
@@ -1,73 +1,90 @@
-#include<iostream>
-#include<cstdlib>
-#include<string>
-#include"buff.h"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include "buff.h"
 
 using namespace std;
 
 int main(){
-	cout << "START TESTING RING BUFFER CLASS - int" << endl << endl;
+	cout << endl << "  START TESTING RING BUFFER CLASS - int" << endl << endl;
 	
 	//init, capacity & size
 	int cap = 4;
-	buffer<int> sensonic(cap);
-	cout << "Initialized ring buffer with capacity = " << cap << endl;
-	cout << "Testing Capacity: returning buffer capacity = " << sensonic.Capacity() << endl;
-	cout << "Testing Size: returning buffer current size = " << sensonic.Size() << endl << endl;
+	buffer<int> myTest(cap);
+	cout << "  Initialized ring buffer with capacity = " << cap << endl;
+	cout << "  Testing Capacity method: returning buffer capacity = " << myTest.Capacity() << endl;
+	cout << "  Testing Size method: returning buffer current size = " << myTest.Size() << endl << endl;
 	
 	//get - empty buffer
-	cout << "Testing Get with empty buffer: (NULL value is assigned)"  << endl;
-	int val = sensonic.Get();
-	cout << "Printing returned value from Get() = " << val << endl << endl;
+	cout << "  Testing Get method (empty buffer): (NULL value is assigned)"  << endl;
+	int val = myTest.Get();
+	cout << "  Printing returned value from Get() = " << val << endl << endl;
 
 	//put & print. fill buffer
-	cout << "Testing Put: adding element in crescent order " << endl;
-	cout << "Print(bool var) was designed to see buffer status.  " << endl;
-	cout << "If var is set to true it also print the eta of each element" << endl;
-	sensonic.Put(1);
-	sensonic.Print(false);
-	cout << "Returning buffer current size: " << sensonic.Size() << endl;
-	sensonic.Put(2);
-	sensonic.Print(false);
-	cout << "Returning buffer current size: " << sensonic.Size() << endl;
-	sensonic.Put(3);
-	sensonic.Print(false);
-	cout << "Returning buffer current size: " << sensonic.Size() << endl;
-	sensonic.Put(4);
-	sensonic.Print(false);
-	cout << "Returning buffer current size: " << sensonic.Size() << endl << endl;
+	cout << "  Testing Put method " << endl;
+	myTest.Put(1);
+	myTest.Print();
+	myTest.Put(2);
+	myTest.Print();
+	myTest.Put(3);
+	myTest.Print();
+	cout << "  Returning buffer current size: " << myTest.Size() << endl;
+	
+	
+	cout << "\n  Testing Get method" << endl;
+	val = myTest.Get();
+	cout << "  Printing returned value from Get() = " << val << endl;
+	myTest.Print();
+	val = myTest.Get();
+	cout << "  Printing returned value from Get() = " << val << endl;
+	myTest.Print();
+	val = myTest.Get();
+	cout << "  Printing returned value from Get() = " << val << endl;
+	cout << endl;
+	
+	cout << "  Filling buffer again" << endl;
+	myTest.Put(1);
+	myTest.Put(2);
+	myTest.Put(3);
+	myTest.Put(4);
+	myTest.Print();
+	cout << "  Returning buffer current size: " << myTest.Size() << endl << endl;
 
 	//put with full buffer
-	cout << "Buffer is now full. Let's see eta and further test Put" << endl;
-	sensonic.Print(true);
-	cout << "Add new element with Put: replace oldest member (highest eta)" << endl;
-	sensonic.Put(5);
-	sensonic.Print(true);
-	cout << "Testing Size: Printing buffer current size: " << sensonic.Size() << endl;
-	cout << "Add new element with Put: replace oldest member (highest eta)" << endl;
-	sensonic.Put(6);
-	sensonic.Print(true);
+	cout << "  Buffer is now full" << endl;
+	cout << "  Testing Put method (full buffer)" << endl;
+	cout << "  Oldest element is replaced each time" << endl;
+	myTest.Put(5);
+	myTest.Print();
+	myTest.Put(6);
+	myTest.Print();
+	myTest.Put(7);
+	myTest.Print();
+	myTest.Put(8);
+	myTest.Print();
+	myTest.Put(9);
+	myTest.Print();
+	myTest.Put(10);
+	myTest.Print();
 	cout << endl;
 	
 	//get with non-empty buffer
-	cout << "Testing Get with non-empty buffer" << endl;
-	cout << "Buffer initial status: ";
-	sensonic.Print(false);
-	val = sensonic.Get();
-	cout << "Printing returned value from Get() = " << val << endl;
-	sensonic.Print(false);
-	val = sensonic.Get();
-	cout << "Printing returned value from Get() = " << val << endl;
-	sensonic.Print(false);
-	val = sensonic.Get();
-	cout << "Printing returned value from Get() = " << val << endl;
-	sensonic.Print(false);
-	val = sensonic.Get();
-	cout << "Printing returned value from Get() = " << val << endl;
-	sensonic.Print(false); 
+	cout << "  Testing Get method (full buffer)" << endl;
+	val = myTest.Get();
+	cout << "  Printing returned value from Get() = " << val << endl;
+	myTest.Print();
+	val = myTest.Get();
+	cout << "  Printing returned value from Get() = " << val << endl;
+	myTest.Print();
+	val = myTest.Get();
+	cout << "  Printing returned value from Get() = " << val << endl;
+	myTest.Print();
+	val = myTest.Get();
+	cout << "  Printing returned value from Get() = " << val << endl << endl;
+	myTest.Print(); 
 	cout << endl;
 
-	cout << "TEST IS OVER!" << endl;
+	cout << "  TEST IS OVER!" << endl << endl;
 	
 	return 0;
 }
